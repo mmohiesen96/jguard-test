4c4740f030bcb7034be3723192f78fd17db68a6d
abdfrehat <abood512543@gmail.com>
2024-02-29 12:29:30 +0300

    test

---
+++ jguard-test-module/src/main/java/com/jguard/test/Document.java
@@ -0,0 +1,15 @@
+package com.jguard.test;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.Documented;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Document
+public class Document {
+
+
+}
---
+++ jguard-test-module/src/main/java/com/jguard/test/Exception.java
@@ -0,0 +1,7 @@
+package com.jguard.test;
+
+
+public class Exception extends Exception {
+
+
+}
