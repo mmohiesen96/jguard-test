33c83703d45a69f9dfa14b6a5dd3154f7ffc5bb9
Mohammed Mohiesen <moh@Mohammeds-MacBook-Pro.local>
2024-03-06 14:40:05 +0300

    add spaces to main method

--- jguard-test-module/src/main/java/com/jguard/test/Main.java
+++ jguard-test-module/src/main/java/com/jguard/test/Main.java
@@ -1,6 +1,8 @@
 package com.jguard.test;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.jguard.test"})
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
